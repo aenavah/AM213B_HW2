a2fdf5c9153b40a28f6c35cb3c2321a313c01cf8
Alexandra Nava <aenava@ucsc.edu>
2024-04-26 18:28:54 -0700

    m

--- HW2_Driver.py
+++ HW2_Driver.py
@@ -1,7 +1,12 @@
 import numpy as np 
+import matplotlib.pyplot as plt
 import HW2_Functions
 
 find_deltatstar = HW2_Functions.find_deltatstar
+plot_boundary = HW2_Functions.plot_boundary
+AB3 = HW2_Functions.AB3
+RK3 = HW2_Functions.RK3
+RK3_absolute_stability_region = HW2_Functions.RK3_absolute_stability_region
 
 '''
 Question 1:
@@ -14,6 +19,69 @@ A = np.array([[0, 10, -10],
 def AB3_boundary(theta):
   y = (12*np.e**(3j*theta) - 12*np.e**(2j*theta))/(5-16*np.e**(theta*1j)+23*np.e**(2j*theta)) 
   return y
-delta_t_star = find_deltatstar(A, AB3_boundary, "Three-step Adams-Bashforth", 1)
+delta_t_star = find_deltatstar(A, AB3_boundary, "Three-step Adams-Bashforth")
 
 
+'''
+Question 2b
+plot the boundary of absolute stability region for BDF3
+'''
+plt.clf()
+def BDF3_boundary(theta):
+  y = (11*np.e**(3j*theta) - 18*np.e**(2j*theta) + 9*np.e**(1j*theta) - 2)/(6*np.e**(3j*theta))
+  return y
+plot_boundary(BDF3_boundary)
+plt.title("BDF3 Absolute Stability Region")
+#plt.show()
+
+'''
+Question 1c
+'''
+for dt in [10**(-4), delta_t_star + 0.0001, delta_t_star - 0.0001]:
+  T = 10
+  n = int(T/dt)
+  ts = np.arange(0, 10, dt)
+  ts = ts[0 : n]
+  u = np.zeros((n, 3))
+
+  u[0] = np.array([10, 10, 10]) 
+
+  #call RK method from Hw1
+  for i in range(1, 2+1):
+    u_next = RK3(A, u[i-1], dt)
+    u[i] = u_next
+    u_last = u_next
+
+  #calling next iterations from AB3 method
+  for i in range(3, n):
+    u_2 = u[i - 1]
+    u_1 = u[i - 2]
+    u_0 = u[i - 3]
+
+    u_3 = AB3(A, u_0, u_1, u_2, dt)
+    u[i] = u_3
+  
+  dy1 = u[:, 0]
+  dy2 = u[:, 1]
+  dy3 = u[:, 2]
+
+  plt.clf()
+  plt.plot(ts, dy1, label = "dy_1")
+  plt.plot(ts, dy2, label = "dy_2")
+  plt.plot(ts, dy3, label = "dy_3")
+  plt.legend()
+  plt.title("delta_t = " + str(dt))
+  plt.savefig("AM3_Solution_dt=" + str(dt) + ".jpg")
+
+plt.clf()
+'''Q4b'''
+A = np.array([[0, 0, 0], 
+              [1/2, 0, 0], 
+              [0, 3/4, 0]])
+b = np.array([[2/9],[1/3],[4/9]])
+h = np.ones_like(b)
+print(b.shape)
+print(h.shape)
+RK3_absolute_stability_region(A, b, h)
+
+  
--- HW2_Functions.py
+++ HW2_Functions.py
@@ -3,8 +3,10 @@ import matplotlib.pyplot as plt
 from math import atan2
 
 
-def find_deltatstar(matrix, method, method_name):
-  eigenvalues = np.linalg.eigvals(matrix)
+def plot_boundary(method):
+  #plots boundary given the boundary function
+  # note: need to plt.title and plt.show after calling this function
+  plt.clf()
   thetas = np.linspace(0, 2*np.pi)
 
   real_vals = []
@@ -21,6 +23,12 @@ def find_deltatstar(matrix, method, method_name):
   plt.xlabel("Re(z)")
   plt.ylabel("Im(z)")
   plt.plot(real_vals, imag_vals)  
+
+
+def find_deltatstar(matrix, method, method_name):
+#finds delta t star given the ODE and the absolute stability boundary
+  eigenvalues = np.linalg.eigvals(matrix)
+  plot_boundary(method)
 
   #iterating through delta_ts 
   for delta_t in np.arange(1, 0, -10**(-5)):
@@ -49,17 +57,50 @@ def find_deltatstar(matrix, method, method_name):
 
     #if all eigenvalues are in region:
     if len(lambda_scaled_reals) == len(eigenvalues):
-      print(lambda_scaled_reals)
-      print(delta_t)
-      plt.scatter(lambda_scaled_reals, lambda_scaled_imags, marker = "o", color = "red", facecolors = "none")
+      #print(lambda_scaled_reals)
+      #print(delta_t)
+      plt.scatter(lambda_scaled_reals, lambda_scaled_imags, marker = "o", color = "red", facecolors = "none", label = "Eigenvalues")
+      plt.legend()
       plt.title(method_name + " with delta_t = " + str(delta_t) )
       plt.show()
-      return
+      return delta_t
 
+def f(A, y):
+  f = A @ y
+  return f 
 
+def RK3(A, y0, dt): 
+    '''RK method from HW1'''
+    K1 = f(A, y0)
+    K2 = f(A, y0 + ((1.0/3.0) * dt * K1))
+    K3 = f(A, y0 + ((2.0/3.0) * dt * K2))
+    y_next = y0 + (dt * (1.0/4.0)) * (K1 + 3.0 * K3)
+    return y_next
 
-
-      
-
-
+def AB3(A, u_k, u_k1, u_k2, delta_t):    
+  u_k3 = u_k2 + delta_t*(1/12)*(23*f(A, u_k2) - 16*f(A, u_k1) + 5*f(A, u_k))
+  return u_k3
   
+
+def RK3_S(A, b, h, x, y):
+  z = complex(x,y)
+  S = abs(np.linalg.det(np.eye(3) - (z*A) + z*(h@(b.T))))
+  return S
+
+def RK3_absolute_stability_region(A, b, h):
+  nx, ny = (200, 200)
+  imag = np.linspace(-4, 4, nx)
+  real = np.linspace(-4, 4, ny)
+  real_v, imag_v = np.meshgrid(real, imag)
+
+  S_grid = np.ones((nx, ny))
+
+  for x_i in range(nx):
+    for y_i in range(ny):
+      boundary = RK3_S(A, b, h, real_v[x_i, y_i], imag_v[x_i, y_i])
+      S_grid[x_i, y_i] = boundary 
+
+  plt.contour(real_v, imag_v, S_grid, [1])
+  plt.grid()
+  plt.title("Boundary of RK3 Method")
+  plt.savefig("RK3_Boundary.jpg")
